230ee15a479c8723d903518c10dcb0c70fcf2d29
Valentin Basin <zudin116@gmail.com>
2025-07-26 16:18:36 +0300

    fix backend

---
+++ backend/config/middlewares.ts
@@ -0,0 +1,21 @@
+export default [
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      origin: [process.env.FRONTEND_URL],
+      headers: "*",
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+];
